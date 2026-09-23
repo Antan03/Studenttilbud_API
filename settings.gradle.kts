6c398030c4ae278809cf8d9974747352341dfc2a
Anders Andersen Tanberg <anderata@uio.no>
2026-09-23 12:12:37 +0200

    Init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "Studenttilbud_API"
---
+++ src/main/kotlin/antan/studenttilbud_api/StudenttilbudApiApplication.kt
@@ -0,0 +1,11 @@
+package antan.studenttilbud_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudenttilbudApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudenttilbudApiApplication>(*args)
+}
